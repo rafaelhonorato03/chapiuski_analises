2c5db6fa1892948728709b7e11671d1b07f6aa4a
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-05-31 09:47:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd()
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
