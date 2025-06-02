28c942cadcef32a929a0ec9a0376069a388a507f
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-06-02 15:08:46 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQqKawlrhvZxCUepOzcl4jG9ejActoqNd11Hs6hDverwxV0gv9PRYjwVxs6coMWsoopfH41EuSLRN-v/pub?gid=0&single=true&output=csv'
-
-df = pd.read_csv(url)
-
-print(df)
